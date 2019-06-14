0d837edafd6992d013125d2b2ab06cafc018e97e
DMayanja21 <you@example.com>
2019-06-14 11:27:32 +0300

    add

--- null_checker.js
+++ null_checker.js
@@ -1,10 +1,13 @@
 for (i=0; i<10; i++)
 {
 	let x=prompt('Pick any number!','');
-		if(x>0)
+		if(x>0){
 			console.log('1');
-		else if (x<0)
+		}
+		else if (x<0){
 			console.log('-1');
-		else
-			console.log('o');
+		}
+		else{
+			console.log('0');
+		}
 }
